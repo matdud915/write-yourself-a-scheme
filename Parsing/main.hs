d333f1700d9163b34824d4a53fc91b4a1612a864
mateusz <matdud915@gmail.com>
2020-11-19 21:53:54 +0100

    variables

--- Parsing/main.hs
+++ Parsing/main.hs
@@ -8,6 +8,8 @@ import Parsers.ExpressionParser (parseExpr)
 import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec (parse)
 import System.IO
+import Data.IORef
+import Variables
 
 flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
@@ -15,11 +17,11 @@ flushStr str = putStr str >> hFlush stdout
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
-evalString :: String -> IO String
-evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+evalString :: Env -> String -> IO String
+evalString env expr = runIOThrows $ liftM show $ (liftThrows $ readExpr expr) >>= eval env
 
-evalAndPrint :: String -> IO ()
-evalAndPrint expr = evalString expr >>= putStrLn
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = evalString env expr >>= putStrLn
 
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 until_ pred prompt action = do
@@ -28,8 +30,11 @@ until_ pred prompt action = do
           then return ()
           else action result >> until_ pred prompt action
 
+runOne :: String -> IO () 
+runOne expr =  nullEnv >>= flip evalAndPrint expr
+
 runRepl :: IO ()
-runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+runRepl = nullEnv >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
@@ -41,5 +46,5 @@ main = do
   args <- getArgs
   case length args of
     0 -> runRepl
-    1 -> evalAndPrint $ args !! 0
+    1 -> runOne $ args !! 0
     _ -> putStrLn "Program takes only 0 or 1 argument"
